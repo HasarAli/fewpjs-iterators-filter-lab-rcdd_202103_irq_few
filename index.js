a7f2246cf82bf8a413a1446c571f9f099944a489
Hasar Ali <hasar.ali@protonmail.com>
2021-05-23 23:18:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 function findMatching(drivers, name) {
   return drivers.filter(driver => driver.toLowerCase().includes(name.toLowerCase()))
 }
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.find(driver => letters.toLowerCase().includes(driver.charAt(0).toLowerCase()))
+}
